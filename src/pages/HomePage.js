229d001bd99d4922eb35f59526d701a361b7163a
arif fadhillah <ariffadhillah319@gmail.com>
2021-06-08 07:14:52 -0400

    Just Arrived

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Header from 'parts/Header'
 import Hero from 'parts/Hero'
+import JustArrived from 'parts/HomePage/JustArrived'
+
 
 
 export default function HomePage(props) {
@@ -9,6 +11,7 @@ export default function HomePage(props) {
         <>
             <Header />
             <Hero />
+            <JustArrived />
         </>
     )
 }
