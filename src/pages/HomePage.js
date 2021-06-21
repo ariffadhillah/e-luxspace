37db9a644c96a756877f42cbdff5ebb9aec70b02
arif fadhillah <ariffadhillah319@gmail.com>
2021-06-20 21:28:52 -0400

    useScrollAnchor

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
 import Header from 'parts/Header'
-import Hero from 'parts/Hero'
+import Hero from 'parts/HomePage/Hero'
 import BrowseRoom from 'parts/HomePage/BrowseRoom'
 import JustArrived from 'parts/HomePage/JustArrived'
 import Clients from 'parts/Clients'
 import Sitemap from 'parts/Sitemap'
 import Footer from 'parts/Footer'
+import useScrollAnchor from 'helper/hooks/useScrollAnchor'
 
 
 
 export default function HomePage(props) {
+    useScrollAnchor();
+    
     return (
         <>
             <Header theme="white" position="absolute" />
